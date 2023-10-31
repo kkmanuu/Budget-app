743c848300023df2d3146ac6eb1c68c99a301d88
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-10-31 14:01:09 +0300

    Modify the development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -64,7 +65,6 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
-
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
