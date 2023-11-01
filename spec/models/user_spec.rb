2eb8b077bc8d945d337ba5dbf0068fe79c11f323
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-01 19:15:06 +0300

    modify the spec user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with a name' do
-    user = User.new(name: 'John Doe')
-    expect(user).to be_valid
-  end
-
-  it 'is not valid without a name' do
-    user = User.new(name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
-  end
-
-  it 'has many categories' do
-    user = User.reflect_on_association(:categories)
-    expect(user.macro).to eq(:has_many)
-  end
-
-  it 'has many purchases' do
-    user = User.reflect_on_association(:purchases)
-    expect(user.macro).to eq(:has_many)
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
