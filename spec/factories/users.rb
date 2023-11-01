4dae1beaf4168ecdbc6780c4c9ce6f949a1ba8e0
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-01 19:14:26 +0300

    Add FactoryBot user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { 'MyString' }
+    name { 'Mission' }
+    email { 'mission@gmail.com' }
+    password { 'password6666' }
+    password_confirmation { 'password6666' }
   end
 end
