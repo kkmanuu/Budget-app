bf51594dc08da01b3ede7d86501420bc6b6c7044
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-02 17:34:17 +0300

    Update the deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
-
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
